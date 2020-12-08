b1ce37a31ec449ecb5260c543799570054094e68
Kim Chanwoo <chanu@snu.ac.kr>
2020-12-09 05:21:26 +0900

    first commit

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 class="asdf">{{msg}}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+$color: red;
+.asdf {
+    color: $color;
+}
+</style>
